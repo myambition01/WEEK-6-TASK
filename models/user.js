1e57178b0fd5600c6ec9bf6ff4f6f3d5ab13ca42
Ibonye <85228725+myambition01@users.noreply.github.com>
2021-07-18 14:39:54 -0700

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,46 @@
+let user = [
+    {
+        id: 1,
+        email: "isiomaibonye@gmail.com",
+        fullName: "Ibonye Isioma",
+        password: "01033789"
+    },
+
+    {
+        id: 2,
+        email: "raymondibonye@yahoo.com",
+        fullName: "Razor Ray",
+        password: "08011187"
+    },
+
+    {
+        id: 3,
+        email: "patrickhox123@yahoo.com",
+        fullName: "Patrick Hox",
+        password: "235432654"
+    },
+
+    {
+        id: 4,
+        email: "juliet1994@gmail.com",
+        fullName: "Juliet Ekwemonye",
+        password: "97988756"
+    },
+
+    {
+        id: 5,
+        email: "kenneth01@yahoo.co.uk",
+        fullName: "Kenneth Banku",
+        password: "87654321"
+    },
+
+    {
+        id: 6,
+        email: "richy@yahoo.com",
+        fullName: "Richard Oboh",
+        password: "87612345"
+    },
+
+];
+
+module.exports = user;
